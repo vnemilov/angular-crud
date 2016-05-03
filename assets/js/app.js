eec75e3b05a93cd2175c130999c27cdfbf846a61
vnemilov <vnemilov@yahoo.com>
2016-05-03 14:57:22 +0300

    Changed ngRoute to ui-router also using controller-as

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,77 @@
-angular.module('CrudApp', []).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/', {templateUrl: 'assets/tpl/lists.html', controller: ListCtrl}).
-      when('/add-user', {templateUrl: 'assets/tpl/add-new.html', controller: AddCtrl}).
-      when('/edit/:id', {templateUrl: 'assets/tpl/edit.html', controller: EditCtrl}).
-      otherwise({redirectTo: '/'});
-}]);
+var shopApp = angular.module('shopApp', ['ui.router']);
+shopApp.config(function($stateProvider, $urlRouterProvider) {
 
-function ListCtrl($scope, $http) {
-  $http.get('api/users').success(function(data) {
-    $scope.users = data;
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+//STATES
+.state('home', {
+  url : '/',
+  templateUrl : 'assets/tpl/lists.html',
+  controller: 'ListCtrl'
+}).state('add-user', {
+  url : '/add-user',
+  templateUrl : 'assets/tpl/add-new.html',
+  controller : 'AddCtrl'
+}).state('/edit/:id', {
+  url : '/edit/:id',
+  templateUrl : 'assets/tpl/edit.html',
+  controller : 'EditCtrl'
+})
+});
+
+
+//LIST CONTROLLER
+//-----------------------------------------------------------------------------------------------------------------------------------------
+shopApp.controller('ListCtrl', function($scope, $http){
+  var vm = this;
+  $http.get('api/users').success(function(data){
+    vm.users = data;
   });
-}
+});
 
-function AddCtrl($scope, $http, $location) {
+
+//ADD CONTROLLER
+//-----------------------------------------------------------------------------------------------------------------------------------------
+shopApp.controller('AddCtrl', function($scope, $http, $state){
   $scope.master = {};
   $scope.activePath = null;
-
-  $scope.add_new = function(user, AddNewForm) {
-
+  $scope.add_new = function(user, AddNewForm){
     $http.post('api/add_user', user).success(function(){
       $scope.reset();
-      $scope.activePath = $location.path('/');
+      $state.go('home');
     });
-
-    $scope.reset = function() {
+    $scope.reset = function(){
       $scope.user = angular.copy($scope.master);
     };
-
     $scope.reset();
-
   };
-}
 
-function EditCtrl($scope, $http, $location, $routeParams) {
-  var id = $routeParams.id;
+});
+
+
+//EDIT CONTROLLER
+//-----------------------------------------------------------------------------------------------------------------------------------------
+shopApp.controller('EditCtrl', function($scope, $http, $location, $stateParams, $state){
+  var vm = this;
+  var id = $stateParams.id;
   $scope.activePath = null;
-
-  $http.get('api/users/'+id).success(function(data) {
-    $scope.users = data;
+  $http.get('api/users/'+id).success(function(data){
+    vm.users = data;
   });
-
   $scope.update = function(user){
-    $http.put('api/users/'+id, user).success(function(data) {
-      $scope.users = data;
-      $scope.activePath = $location.path('/');
+    $http.put('api/users/'+id, user).success(function(data){
+      vm.users = data;
     });
+    $state.go('home');
   };
-
-  $scope.delete = function(user) {
+  $scope.delete = function(user){
     console.log(user);
-
-    var deleteUser = confirm('Are you absolutely sure you want to delete?');
-    if (deleteUser) {
+    var deleteUser = confirm('You sure?');
+    if(deleteUser){
       $http.delete('api/users/'+user.id);
-      $scope.activePath = $location.path('/');
+      $state.go('home');
     }
   };
-}
+});
